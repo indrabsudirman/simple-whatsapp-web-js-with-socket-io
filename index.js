4ba7d609e67666488b421cd1c2cb90911d8902ca
indrabsudirman <indrabsudirman@gmail.com>
2024-01-03 22:04:34 +0700

    Add Remote Auth to MongoDB

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 import express from "express";
 import pkg from "whatsapp-web.js";
-const { Client, LocalAuth } = pkg;
+const { Client, LocalAuth, RemoteAuth } = pkg;
 const app = express();
 const port = 3000;
 
 import http from "http";
 const server = http.createServer(app);
 import { Server } from "socket.io";
+import { MongoStore } from "wwebjs-mongo";
+import mongoose from "mongoose";
 const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
+});
+const MONGO_DB_LOCAL = "mongodb://localhost:27017/whatsapp-web-with-chatgpt";
+let store;
+
+mongoose.connect(MONGO_DB_LOCAL).then(() => {
+  console.log(`Connected to MongoDB`);
+  store = new MongoStore({ mongoose: mongoose });
+  console.log(store);
 });
 
 app.get("/", (req, res) => {
@@ -29,8 +39,10 @@ const createWhatsappSession = (id, socket) => {
     puppeteer: {
       headless: true,
     },
-    authStrategy: new LocalAuth({
+    authStrategy: new RemoteAuth({
       clientId: id,
+      store: store,
+      backupSyncIntervalMs: 300000,
     }),
   });
 
@@ -51,7 +63,37 @@ const createWhatsappSession = (id, socket) => {
     socket.emit("ready", { id, message: `Client ${client} is ready` });
   });
 
+  client.on("remote_session_saved", () => {
+    console.log(`Remote session saved`);
+  });
+
   client.initialize();
+};
+
+const getWhatsappSession = (id, socket) => {
+  const client = new Client({
+    puppeteer: {
+      headless: true,
+    },
+    authStrategy: new RemoteAuth({
+      clientId: id,
+      store: store,
+      backupSyncIntervalMs: 300000,
+    }),
+  });
+
+  client.on("ready", () => {
+    console.log(`Client is ready`);
+    socket.emit("ready", { id, message: `Client ${client} is ready` });
+  });
+
+  client.on("qr", (qr) => {
+    console.log("QR RECEIVED", qr);
+    socket.emit("qr", {
+      qr,
+      message: "Here is the QR, because user has logged out",
+    });
+  });
 };
 
 io.on("connection", (socket) => {
@@ -72,5 +114,11 @@ io.on("connection", (socket) => {
     const { id } = data;
     console.log(`data ${id}`);
     createWhatsappSession(id, socket);
+  });
+
+  socket.on("getSession", (data) => {
+    console.log(`Data is ${data}`);
+    const { id } = data;
+    getWhatsappSession(id, socket);
   });
 });
